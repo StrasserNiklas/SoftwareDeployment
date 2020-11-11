381ad076d6c5ab7cbe0b5656a5fca302e0965095
StrasserNiklas <niklasstrasser@hotmail.com>
2020-11-11 18:53:33 +0100

    next

--- Webapp/index.js
+++ Webapp/index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT||3000;
+
+let appInsights = require("applicationinsights");
+appInsights
+  .setup("07da5ab1-3d16-46d4-a94b-748bb8ba3d53")
+  .start()
+
+app.set("view engine", "jade");
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+  res.render("index");
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
